ba06b756b6212654d8c8057ac1243f83da75c248
Kenk <kenghin_lim@hotmail.com>
2023-11-18 19:10:09 +0300

    Add UrqlProvider and graphClient

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -27,8 +27,10 @@ import toast, { Toaster } from "react-hot-toast";
 import type { AppProps } from "next/app";
 import ChainContext from "@/context/Chain";
 import { ThemeProvider } from "styled-components";
-import { useState } from "react";
+import { Provider as UrqlProvider } from "urql";
 import { Web3Modal } from "../context/Web3Modal";
+import { graphClient } from "@/utils/graph";
+import { useState } from "react";
 
 export const metadata = {
   title: "Web3Modal",
@@ -53,48 +55,50 @@ export default function App({ Component, pageProps }: AppProps) {
   // const [selectedChain, setSelectedChain] = useState<any>(Mumbai);
 
   return (
-    <ThirdwebProvider
-      supportedWallets={[
-        metamaskWallet(),
-        smartWallet(
-          embeddedWallet({
-            auth: {
-              options: ["email", "google"],
-            },
-          }),
-          config
-        ),
-        // safeWallet({
-        //   personalWallets: [
-        //     embeddedWallet({
-        //       auth: {
-        //         options: ["email", "google"],
-        //       },
-        //     }),
-        //   ],
-        // }),
-      ]}
-      clientId={process.env.NEXT_PUBLIC_CLIENT_ID}
-      activeChain={Mumbai}
-      supportedChains={[
-        Mumbai,
-        ArbitrumSepolia,
-        PolygonZkevmTestnet,
-        LineaTestnet,
-        ScrollSepoliaTestnet,
-        GnosisChiadoTestnet,
-        MantleTestnet,
-        CeloAlfajoresTestnet,
-        BaseSepoliaTestnet,
-      ]}
-    >
-      <ThemeProvider theme={theme}>
-        <ThorinGlobalStyles />
-        <Web3Modal>
-          <Component {...pageProps} />
-        </Web3Modal>
-      </ThemeProvider>
-      <Toaster />
-    </ThirdwebProvider>
+    <UrqlProvider value={graphClient}>
+      <ThirdwebProvider
+        supportedWallets={[
+          metamaskWallet(),
+          smartWallet(
+            embeddedWallet({
+              auth: {
+                options: ["email", "google"],
+              },
+            }),
+            config
+          ),
+          // safeWallet({
+          //   personalWallets: [
+          //     embeddedWallet({
+          //       auth: {
+          //         options: ["email", "google"],
+          //       },
+          //     }),
+          //   ],
+          // }),
+        ]}
+        clientId={process.env.NEXT_PUBLIC_CLIENT_ID}
+        activeChain={Mumbai}
+        supportedChains={[
+          Mumbai,
+          ArbitrumSepolia,
+          PolygonZkevmTestnet,
+          LineaTestnet,
+          ScrollSepoliaTestnet,
+          GnosisChiadoTestnet,
+          MantleTestnet,
+          CeloAlfajoresTestnet,
+          BaseSepoliaTestnet,
+        ]}
+      >
+        <ThemeProvider theme={theme}>
+          <ThorinGlobalStyles />
+          <Web3Modal>
+            <Component {...pageProps} />
+          </Web3Modal>
+        </ThemeProvider>
+        <Toaster />
+      </ThirdwebProvider>
+    </UrqlProvider>
   );
 }
