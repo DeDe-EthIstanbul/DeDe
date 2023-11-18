e183db922b77474574854aeceb9d45b61fc6e4f5
Eason Chai <echai2905@gmail.com>
2023-11-19 01:40:53 +0300

    feat: Update contract addresses

--- packages/frontend/src/utils/addresses.ts
+++ packages/frontend/src/utils/addresses.ts
@@ -36,10 +36,10 @@ export const getDeDeContractAddress = (
       contractAddress = "0xdf48692003Dd581b7E3715A26a6244f49B2e56da"; // Replace with the actual Linea contract address
       break;
     case 10200: // Gnosis
-      contractAddress = "0x1234567890"; // Replace with the actual Gnosis contract address
+      contractAddress = "0xdf48692003Dd581b7E3715A26a6244f49B2e56da"; // Replace with the actual Gnosis contract address
       break;
-    case 421613: // Arbitrum
-      contractAddress = "0x1234567890"; // Replace with the actual Arbitrum contract address
+    case 421614: // Arbitrum
+      contractAddress = "0xdf48692003Dd581b7E3715A26a6244f49B2e56da"; // Replace with the actual Arbitrum contract address
       break;
     case 5: // Goerli
       contractAddress = "0x1234567890"; // Replace with the actual Goerli contract address
@@ -48,7 +48,7 @@ export const getDeDeContractAddress = (
       contractAddress = "0xdf48692003Dd581b7E3715A26a6244f49B2e56da"; // Replace with the actual Goerli contract address
       break;
     case 1442: // Polygon zkEVM
-      contractAddress = "0x1234567890"; // Replace with the actual Goerli contract address
+      contractAddress = "0xdf48692003Dd581b7E3715A26a6244f49B2e56da"; // Replace with the actual Goerli contract address
       break;
     // Add more cases for other chainIds if needed
     default:
