fc3640de6b33b9d7e364621cbc6fad7c678d1dcd
Kenk <kenghin_lim@hotmail.com>
2023-11-18 03:05:51 +0300

    Fix EAS_ADDRESS values in deployment script

--- packages/contracts/deploy/1_DeDe.ts
+++ packages/contracts/deploy/1_DeDe.ts
@@ -15,31 +15,30 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }: any) => {
   } else if (chainId == '42') { // base testnet
     EAS_ADDRESS = '0x4200000000000000000000000000000000000021';
   } else if (chainId == '42') { // scroll testnet
-    EAS_ADDRESS = '0x0000000000000000000000000000000000000000';
+    EAS_ADDRESS = '0x0000000000000000000000000000000000000001';
     console.log('EAS Not available on Scroll Testnet')
   } else if (chainId == '42') { // linea testnet
     EAS_ADDRESS = '0xaEF4103A04090071165F78D45D83A0C0782c2B2a';
   } else if (chainId == '42') { // mantle testnet
-    EAS_ADDRESS = '0x0000000000000000000000000000000000000000';
+    EAS_ADDRESS = '0x0000000000000000000000000000000000000001';
     console.log('EAS Not available on Mantle Testnet');
   } else if (chainId == '10200') { // gnosis testnet
-    EAS_ADDRESS = '0x0000000000000000000000000000000000000000';
+    EAS_ADDRESS = '0x0000000000000000000000000000000000000001';
     console.log('EAS Not available on Gnosis Testnet');
   } else if (chainId == '421613') { // arbitrum testnet
     EAS_ADDRESS = '0xaEF4103A04090071165F78D45D83A0C0782c2B2a';
   } else if (chainId == '80001') { // mumbai testnet
     EAS_ADDRESS = '0xaEF4103A04090071165F78D45D83A0C0782c2B2a';
   } else {
-    EAS_ADDRESS = '0x0000000000000000000000000000000000000000';
+    EAS_ADDRESS = '0x0000000000000000000000000000000000000001';
     console.log('EAS Not available on this network');
   }
-
 
 
   let contract = await deploy('DeDe', {
     from: deployer,
     log: true,
-    args: [0, 0, EAS_ADDRESS], //TODO: set correct values
+    args: [0, EAS_ADDRESS], //TODO: set correct values
   });
 
 
