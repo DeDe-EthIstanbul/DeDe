95b63662e4de5923eceb88ebb960b652bf353b2c
Eason Chai <echai2905@gmail.com>
2023-11-18 16:13:29 +0300

    feat: Add base backend for Waku

---
+++ packages/backend/index.mjs
@@ -0,0 +1,30 @@
+import express from "express";
+import { setup, createWakuEncoder, sendMessage } from "./setup.mjs";
+
+const app = express();
+const port = 3001;
+let node;
+let encoder;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/pickup", async (req, res) => {
+  const data = sendMessage(node, encoder, "Your parcel has been received!");
+  console.log("Message sent");
+  res.send(JSON.stringify(data, null, 4));
+});
+
+app.get("/dropoff", async (req, res) => {
+  const data = sendMessage(node, encoder, "Your parcel has been delivered!");
+  console.log("Message sent");
+  res.send(JSON.stringify(data, null, 4));
+});
+
+app.listen(port, async () => {
+  node = await setup();
+  encoder = await createWakuEncoder();
+
+  console.log(`Backend messaging listening on port ${port}`);
+});
