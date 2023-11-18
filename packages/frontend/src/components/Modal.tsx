33c9f0ee4b7fd0981b46e2ec6bc7be68ae7d8e52
Eason Chai <echai2905@gmail.com>
2023-11-18 13:56:16 +0300

    feat: Fix rendering issue

--- packages/frontend/src/components/Modal.tsx
+++ packages/frontend/src/components/Modal.tsx
@@ -15,10 +15,10 @@ export default function Modal({ isOpen, setIsOpen, children }: IModal) {
       className="relative z-50"
     >
       {/* The backdrop, rendered as a fixed sibling to the panel container */}
-      <div className="fixed inset-0 bg-[#051316]/70" aria-hidden="true" />
+      <div className="fixed inset-0 bg-[#051316]/70 z-20" aria-hidden="true" />
 
       {/* Full-screen container to center the panel */}
-      <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+      <div className="fixed inset-0 flex w-screen items-center justify-center p-4 z-20">
         {/* The actual dialog panel  */}
         <Dialog.Panel className="mx-auto max-w-sm rounded-lg bg-brand-text py-6 px-8 flex flex-col items-center justify-center">
           {children}
