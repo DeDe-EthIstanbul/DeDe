3f0bd545d66a894bfdd75d0a4708e7b3295345f0
Kenk <kenghin_lim@hotmail.com>
2023-11-18 12:18:05 +0300

    add auto redirect to /user page

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 import { ConnectWallet, lightTheme, useAddress } from "@thirdweb-dev/react";
+import { useEffect, useState } from "react";
+
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 
 export default function Home() {
   const [type, setType] = useState<"user" | "courier" | undefined>();
@@ -10,8 +11,8 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    if (type && address) {
-      router.push(`/${type}`);
+    if (address) {
+      router.push(`/user`);
     }
   }, [type, address, router]);
 
@@ -55,41 +56,6 @@ export default function Home() {
               fontFamily: "Poppins, sans-serif",
             })}
             btnTitle={"Sign In As User"}
-            modalTitle={"Sign In To DeDe"}
-            switchToActiveChain={true}
-            modalSize={"compact"}
-            modalTitleIconUrl={"/assets/dede_logo.png"}
-            className="font-bold py-3 w-full font-sans"
-          />
-        </div>
-        <div
-          className="w-full items-center justify-center flex flex-row"
-          onClick={() => setType("courier")}
-        >
-          <ConnectWallet
-            theme={lightTheme({
-              colors: {
-                modalBg: "#FEFEF2",
-                dropdownBg: "#FEFEF2",
-                primaryText: "#022D36",
-                secondaryText: "#659098",
-                borderColor: "#023039",
-                separatorLine: "#023039",
-                accentText: "#023039",
-                accentButtonBg: "#023039",
-                danger: "#F75C5A",
-                success: "#30a46c",
-                primaryButtonBg: "#FAF6DB",
-                accentButtonText: "#FFFEF1",
-                primaryButtonText: "#022D36",
-                secondaryButtonBg: "#97CED9",
-                secondaryButtonText: "#023039",
-                connectedButtonBg: "#FFFEF1",
-                secondaryButtonHoverBg: "#659098",
-              },
-              fontFamily: "Poppins, sans-serif",
-            })}
-            btnTitle={"Register As Courier"}
             modalTitle={"Sign In To DeDe"}
             switchToActiveChain={true}
             modalSize={"compact"}
