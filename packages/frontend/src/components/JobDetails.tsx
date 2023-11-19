3f2cb3d23b642bc93d25043c9e4b62e413dc5c03
Eason Chai <echai2905@gmail.com>
2023-11-19 03:48:38 +0300

    feat: Update mock data and add dede score checks

--- packages/frontend/src/components/JobDetails.tsx
+++ packages/frontend/src/components/JobDetails.tsx
@@ -4,6 +4,10 @@ import LocationIcon from "@/icons/location";
 import StarIcon from "@/icons/star";
 import { useSigner } from "@thirdweb-dev/react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import toast from "react-hot-toast";
+import { Tooltip, TooltipProps } from "@ensdomains/thorin";
+import clsx from "clsx";
 
 interface IJobDetails {
   pickUp: string;
@@ -23,7 +27,8 @@ export default function JobDetails({
   distance,
   userRating,
 }: IJobDetails) {
-  let signer = useSigner();
+  const router = useRouter();
+  const currentDeDeScore = 50;
 
   return (
     <div className="flex flex-col relative">
@@ -34,15 +39,15 @@ export default function JobDetails({
             <p className="font-bold font-sans">Parcel Delivery</p>
             <p className="font-bold font-sans text-brand-success">{revenue}</p>
           </div>
-          <div className="flex flex-row w-full items-center">
-            <div className="flex flex-col basis-1/2">
+          <div className="flex flex-row w-full">
+            <div className="flex flex-col basis-1/2 items-start">
               <div className="flex flex-row items-center">
                 <LocationIcon className="h-4 mr-2 fill-brand-primary" />
                 <p className="font-sans text-sm">Pick Up</p>
               </div>
               <p className="font-bold font-sans text-sm">{pickUp}</p>
             </div>
-            <div className="flex flex-col basis-1/2">
+            <div className="flex flex-col basis-1/2 items-start">
               <div className="flex flex-row items-center">
                 <LocationIcon className="h-4 mr-2 fill-brand-primary" />
                 <p className="font-sans text-sm">Drop Off</p>
@@ -69,11 +74,43 @@ export default function JobDetails({
               <p className="font-sans">{dedeScore} DeDe</p>
             </div>
           </div>
-          <Link href="/courier/pickup">
-            <button className="font-bold w-full bg-brand-primary rounded-lg py-3 text-white font-sans">
+          <Tooltip
+            additionalGap={0}
+            content={
+              currentDeDeScore >= dedeScore ? (
+                <p className="font-sans text-center">You can accept this job</p>
+              ) : (
+                <p className="font-sans text-center">
+                  Your DeDe score is too low
+                </p>
+              )
+            }
+            mobilePlacement="top"
+            mobileWidth={250}
+            placement="top"
+            targetId="buttonIdTop"
+            width={250}
+          >
+            <button
+              className={clsx(
+                "font-bold w-full rounded-lg py-3 text-white font-sans",
+                currentDeDeScore >= dedeScore
+                  ? "bg-brand-primary"
+                  : "bg-brand-secondary cursor-not-allowed"
+              )}
+              onClick={() => {
+                if (currentDeDeScore >= dedeScore) {
+                  router.push("/courier/pickup");
+                } else {
+                  toast.error(
+                    "You do not have enough DeDe Score to pick up this job!"
+                  );
+                }
+              }}
+            >
               Pick Up
             </button>
-          </Link>
+          </Tooltip>
         </Disclosure.Panel>
       </Disclosure>
     </div>
